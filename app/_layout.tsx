0ee873deed646e6ae6536d5074d112c92e4aa557
Yilin Wang <sa22039@student.samk.fi>
2026-02-11 19:58:52 +0200

    create page: back icon only, dont have ‘index’ title

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,12 @@ export default function RootLayout() {
   return (
     <Stack>
       {/* Hide the header for the index screen */}
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false,title: ""}} />
+      <Stack.Screen name="create" options={{ 
+          title: "Add New Item",
+        }} 
+      />
     </Stack>
+    
   );
 }
